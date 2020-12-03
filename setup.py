5174e28e9047f520980b0b8c12a5433da22e92a5
jakeogh <github.com@v6y.net>
2020-12-03 11:04:57 -0700

    auto-commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import fastentrypoints
+from setuptools import find_packages, setup
+if not sys.version_info[0] == 3:
+    sys.exit("Python 3 is required. Use: \'python3 setup.py install\'")
+
+dependencies = ["icecream", "click", "colorama"]
+
+config = {
+    "version": "0.1",
+    "name": "uniquepipe",
+    "url": "https://github.com/jakeogh/uniquepipe",
+    "license": "ISC",
+    "author": "Justin Keogh",
+    "author_email": "github.com@v6y.net",
+    "description": "Short explination of what it does _here_",
+    "long_description": __doc__,
+    "packages": find_packages(exclude=['tests']),
+    "include_package_data": True,
+    "zip_safe": False,
+    "platforms": "any",
+    "install_requires": dependencies,
+    "entry_points": {
+        "console_scripts": [
+            "uniquepipe=uniquepipe.uniquepipe:cli",
+        ],
+    },
+}
+
+setup(**config)
---
+++ uniquepipe/uniquepipe.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+
+# pylint: disable=C0111  # docstrings are always outdated and wrong
+# pylint: disable=W0511  # todo is encouraged
+# pylint: disable=C0301  # line too long
+# pylint: disable=R0902  # too many instance attributes
+# pylint: disable=C0302  # too many lines in module
+# pylint: disable=C0103  # single letter var names, func name too descriptive
+# pylint: disable=R0911  # too many return statements
+# pylint: disable=R0912  # too many branches
+# pylint: disable=R0915  # too many statements
+# pylint: disable=R0913  # too many arguments
+# pylint: disable=R1702  # too many nested blocks
+# pylint: disable=R0914  # too many local variables
+# pylint: disable=R0903  # too few public methods
+# pylint: disable=E1101  # no member for base
+# pylint: disable=W0201  # attribute defined outside __init__
+# pylint: disable=R0916  # Too many boolean expressions in if statement
+
+
+#import os
+import hashlib
+
+import click
+
+try:
+    from icecream import ic  # https://github.com/gruns/icecream
+except ImportError:
+    import sys
+
+    def eprint(*args, **kwargs):
+        if 'file' in kwargs.keys():
+            kwargs.pop('file')
+        print(*args, file=sys.stderr, **kwargs)
+
+from enumerate_input import enumerate_input
+
+
+def generate_truncated_string_hash(string):
+    assert isinstance(string, str)
+    byte_string = string.encode('UTF-8')
+    digest = getattr(hashlib, 'sha3_256')(byte_string).digest()
+    hexdigest = digest.hex()
+    return hexdigest[0:31]
+
+
+class UniquePipe():
+    def __init__(self,
+                 verbose=False,):
+        self.hashes = set()
+
+    def filter(self, string):
+        string_hash = generate_truncated_string_hash(string)
+        if self.verbose:
+            ic(string_hash)
+        if string_hash not in self.hashes:
+            self.hashes.add(string_hash)
+            return True
+        return False
+
+
+@click.command()
+@click.argument("items", type=str, nargs=-1)
+@click.option('--verbose', is_flag=True)
+@click.option('--debug', is_flag=True)
+@click.option("--printn", is_flag=True)
+def cli(items,
+        verbose,
+        debug,
+        printn,):
+
+    null = not printn
+    end = '\n'
+    if null:
+        end = '\x00'
+    if sys.stdout.isatty():
+        end = '\n'
+
+    uniquepipe = UniquePipe()
+
+    for index, item in enumerate_input(iterator=items,
+                                       null=null,
+                                       debug=debug,
+                                       verbose=verbose,):
+        if verbose:
+            ic(index, item)
+
+        if uniquepipe.filter(item):
+            print(item, end=end)
