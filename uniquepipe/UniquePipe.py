6c8235f32db59e4dc7d5821ec49d43d63dbcb109
jakeogh <github.com@v6y.net>
2020-12-03 11:41:03 -0700

    auto-commit

--- uniquepipe/UniquePipe.py
+++ uniquepipe/UniquePipe.py
@@ -52,3 +52,17 @@ class UniquePipe():
             self.hashes.add(string_hash)
             return True
         return False
+
+    def remove(self, string):
+        string_hash = generate_truncated_string_hash(string)
+        if self.verbose:
+            ic(string_hash)
+        self.hashes.pop(string_hash)
+
+    def exists(self, string):
+        string_hash = generate_truncated_string_hash(string)
+        if self.verbose:
+            ic(string_hash)
+        if string_hash in self.hashes:
+            return True
+        return False
