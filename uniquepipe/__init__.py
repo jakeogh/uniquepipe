f626fd8483299f176d4167ed43abad89460dd7eb
jakeogh <github.com@v6y.net>
2021-05-27 18:20:00 -0700

    auto-commit

--- uniquepipe/__init__.py
+++ uniquepipe/__init__.py
@@ -1,1 +1,3 @@
-from .UniquePipe import UniquePipe, generate_truncated_string_hash
+from .UniquePipe import UniquePipe
+from .UniquePipe import generate_truncated_string_hash
+from .UniquePipe import hamming_distance
