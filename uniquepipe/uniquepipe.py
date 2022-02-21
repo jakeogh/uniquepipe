38401908a8a66868fa60b47c320caeedb417eb70
jakeogh <github.com@v6y.net>
2022-02-21 15:12:44 -0700

    auto-commit

--- uniquepipe/uniquepipe.py
+++ uniquepipe/uniquepipe.py
@@ -27,20 +27,20 @@ from clicktool import click_add_options
 from clicktool import click_global_options
 from clicktool import tv
 from eprint import eprint
+# from colorama import Fore
+# from colorama import Style
 from mptool import output
-#from colorama import Fore
-#from colorama import Style
-from unmp import unmp
+from mptool import unmp
 
 from uniquepipe import UniquePipe
 from uniquepipe.UniquePipe import HashAlgorithmError
 
 
 def str_list(line):
-    line = [str(thing) for thing in line]   # angryfiles...
+    line = [str(thing) for thing in line]  # angryfiles...
     if len(line) == 1:
         return line[0]
-    line = ' '.join(line)
+    line = " ".join(line)
     return line
 
 
@@ -49,7 +49,7 @@ def perhaps_invert(thing, *, invert):
         return not thing
 
 
-#def print_list(*, output_list, end, stderr,):
+# def print_list(*, output_list, end, stderr,):
 #    output_list = str_list(output_list)
 #    if stderr:
 #        eprint(output_list, end=end)
@@ -58,76 +58,86 @@ def perhaps_invert(thing, *, invert):
 #
 #
 
+
 @click.command()
 @click.argument("items", type=str, nargs=-1)
-@click.option('--duplicates', is_flag=True)
-@click.option('--paths', is_flag=True, help='hash file contents')
-@click.option('--images', '--image', is_flag=True)
-@click.option('--count', is_flag=True)
-@click.option('--prepend', is_flag=True)
-@click.option('--accept-empty', is_flag=True)
-@click.option('--length', type=int, default=32)
-@click.option('--distance', type=int)
+@click.option("--duplicates", is_flag=True)
+@click.option("--paths", is_flag=True, help="hash file contents")
+@click.option("--images", "--image", is_flag=True)
+@click.option("--count", is_flag=True)
+@click.option("--prepend", is_flag=True)
+@click.option("--accept-empty", is_flag=True)
+@click.option("--length", type=int, default=32)
+@click.option("--distance", type=int)
 @click.option("--show-closest-distance", is_flag=True)
 @click.option("--show-skipped", is_flag=True)
-@click.option("--algorithm", type=str, default='sha3_256')
+@click.option("--algorithm", type=str, default="sha3_256")
 @click.option("--exit-on-collision", is_flag=True)
-@click.option("--preload", "preloads",
-              type=click.Path(exists=True,
-                              dir_okay=False,
-                              file_okay=True,
-                              path_type=str,
-                              allow_dash=True,),
-              multiple=True)
+@click.option(
+    "--preload",
+    "preloads",
+    type=click.Path(
+        exists=True,
+        dir_okay=False,
+        file_okay=True,
+        path_type=str,
+        allow_dash=True,
+    ),
+    multiple=True,
+)
 @click_add_options(click_global_options)
 @click.pass_context
-def cli(ctx,
-        items,
-        duplicates: bool,
-        preloads,
-        verbose: Union[bool, int, float],
-        verbose_inf: bool,
-        count: int,
-        exit_on_collision: bool,
-        length: int,
-        show_closest_distance: int,
-        show_skipped: int,
-        distance: int,
-        accept_empty: bool,
-        algorithm: str,
-        paths: bool,
-        images: bool,
-        prepend: bool,
-        ):
+def cli(
+    ctx,
+    items,
+    duplicates: bool,
+    preloads,
+    verbose: Union[bool, int, float],
+    verbose_inf: bool,
+    count: int,
+    exit_on_collision: bool,
+    length: int,
+    show_closest_distance: int,
+    show_skipped: int,
+    distance: int,
+    accept_empty: bool,
+    algorithm: str,
+    paths: bool,
+    images: bool,
+    prepend: bool,
+    dict_input: bool,
+):
 
-    tty, verbose = tv(ctx=ctx,
-                      verbose=verbose,
-                      verbose_inf=verbose_inf,
-                      )
+    tty, verbose = tv(
+        ctx=ctx,
+        verbose=verbose,
+        verbose_inf=verbose_inf,
+    )
     if images:
-        algorithm = 'pdqhash'
+        algorithm = "pdqhash"
 
     ic(algorithm)
-    if algorithm == 'pdqhash':
+    if algorithm == "pdqhash":
         paths = True
         if not distance:
             distance = 8
-            eprint('Warning: using default distance:', distance)
+            eprint("Warning: using default distance:", distance)
 
-    uniquepipe = UniquePipe(algorithm=algorithm,
-                            length=length,
-                            accept_empty=accept_empty,
-                            distance=distance,
-                            paths=paths,
-                            verbose=verbose,
-                            )
+    uniquepipe = UniquePipe(
+        algorithm=algorithm,
+        length=length,
+        accept_empty=accept_empty,
+        distance=distance,
+        paths=paths,
+        verbose=verbose,
+    )
     for preload in preloads:
         if verbose:
             ic(preload)
-        with open(preload, 'rb') as fh:
+        with open(preload, "rb") as fh:
             for index, item in enumerate(fh):
                 if verbose:
-                    ic('preload:', index, item)
+                    ic("preload:", index, item)
                 try:
                     uniquepipe.filter(item)
                 except HashAlgorithmError as e:
@@ -135,15 +145,21 @@ def cli(ctx,
                         ic(e)
                     continue
         if verbose:
-            ic('preloaded:', len(uniquepipe))
+            ic("preloaded:", len(uniquepipe))
 
     unique_count = 0
     duplicate_count = 0
-    #bytes_read = 0
+    # bytes_read = 0
     if items:
         iterator = items
     else:
-        iterator = unmp(verbose=verbose, valid_types=[str, bytes,])
+        iterator = unmp(
+            verbose=verbose,
+            valid_types=[
+                str,
+                bytes,
+            ],
+        )
 
     for index, item in enumerate(iterator):
         new = False
@@ -153,7 +169,7 @@ def cli(ctx,
             ic(index, item)
 
         if len(item) == 0:
-            ic('empty value found:', index, item, accept_empty)
+            ic("empty value found:", index, item, accept_empty)
 
         try:
             new, distance, digest = uniquepipe.filter(item)
@@ -166,8 +182,14 @@ def cli(ctx,
             unique_count += 1
             if not count:
                 if not duplicates:
-                    output(item, tty=tty, verbose=verbose,)
-                    #print_result(digest=digest,
+                    output(
+                        item,
+                        reason=None,
+                        dict_input=dict_input,
+                        tty=tty,
+                        verbose=verbose,
+                    )
+                    # print_result(digest=digest,
                     #             distance=distance,
                     #             item=item,
                     #             prepend=prepend,
@@ -186,8 +208,14 @@ def cli(ctx,
                 ic(sys.getsizeof(uniquepipe))
                 raise ValueError("collision: {}".format(item))
             if duplicates:
-                output(item, tty=tty, verbose=verbose,)
-                #print_result(digest=digest,
+                output(
+                    item,
+                    reason=None,
+                    dict_input=dict_input,
+                    tty=tty,
+                    verbose=verbose,
+                )
+                # print_result(digest=digest,
                 #             distance=distance,
                 #             item=item,
                 #             prepend=prepend,
@@ -198,8 +226,14 @@ def cli(ctx,
                 #             stderr=False,
                 #             )
             if show_skipped:
-                output(item, tty=tty, verbose=verbose,)
-                #print_result(digest=digest,
+                output(
+                    item,
+                    reason=None,
+                    dict_input=dict_input,
+                    tty=tty,
+                    verbose=verbose,
+                )
+                # print_result(digest=digest,
                 #             distance=distance,
                 #             item=item,
                 #             prepend=prepend,
@@ -212,6 +246,18 @@ def cli(ctx,
 
     if count:
         if duplicates:
-            print(duplicate_count, file=sys.stderr)
+            output(
+                duplicate_count,
+                reason=None,
+                dict_input=dict_input,
+                tty=tty,
+                verbose=verbose,
+            )
         else:
-            print(unique_count, file=sys.stderr)
+            output(
+                unique_count,
+                reason=None,
+                dict_input=dict_input,
+                tty=tty,
+                verbose=verbose,
+            )
